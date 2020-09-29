3661993724bb338a5b28f69763c6ca4b7a1c34d2
ihor5433 <49032918+ihor5433@users.noreply.github.com>
2020-09-29 11:49:00 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+if [[ $EUID -ne 0 ]]; then
+ echo "This script must be run as root" 
+ exit 1
+else
+ #Update and Upgrade
+ echo "Updating and Upgrading"
+ apt-get update && sudo apt-get upgrade -y
+ apt install wget
+
+
+echo "Installing ZSH"
+ apt install zsh
+echo "Instaling OH-MY-ZSH"
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+echo "Copy ZSH-config"
+wget https://github.com/ihor5433/config/blob/master/.alias_zsh -P $HOME
+wget https://github.com/ihor5433/config/blob/master/.zshrc -P $HOME
+
+echo "Configure zsh"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "source /home/ihor/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "source /home/ihor/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
